8fd8db9d1d75df9eb91aeb54dc72b255d9add618
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-07-24 11:01:19 +0200

    Initial commit

---
+++ app/behandlinger/page.tsx
@@ -0,0 +1,72 @@
+import { EChart } from '@/lib/echarts/echarts';
+import { getBehandlinger, getSakstyper } from '@/lib/server/api';
+import { Sakstype } from '@/lib/server/types';
+
+export default async function Page() {
+  const behandlinger = await getBehandlinger();
+  const sakstyper = await getSakstyper();
+
+  const data = Object.values(
+    Object.fromEntries(
+      behandlinger.anonymizedBehandlingList
+        .filter(
+          (b) =>
+            b.typeId !== Sakstype.ANKE_I_TRYGDERETTEN && !b.isAvsluttetAvSaksbehandler && b.feilregistrering === null,
+        )
+        .reduce<Map<Sakstype, { value: number; name: string }>>((acc, curr) => {
+          const existing = acc.get(curr.typeId);
+
+          if (existing) {
+            existing.value += 1;
+          } else {
+            acc.set(curr.typeId, {
+              name: sakstyper.find((s) => s.id === curr.typeId)?.navn ?? (curr.typeId || curr.typeId),
+              value: 1,
+            });
+          }
+          return acc;
+        }, new Map()),
+    ),
+  );
+
+  // const data = [
+  //   { name: 'Klage', value: 1863 },
+  //   { name: 'Anke', value: 235 },
+  //   { name: 'Behandling etter Trygderetten opphevet', value: 1 },
+  //   { name: 'Omgjøringskrav', value: 5 },
+  // ];
+
+  return (
+    <div className="grow">
+      <EChart
+        option={{
+          title: {
+            text: 'Aktive saker',
+            left: 'center',
+          },
+          tooltip: {
+            trigger: 'item',
+          },
+          legend: {
+            orient: 'vertical',
+            left: 'left',
+          },
+          series: [
+            {
+              type: 'pie',
+              radius: '50%',
+              data,
+              emphasis: {
+                itemStyle: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)',
+                },
+              },
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+}
---
+++ components/filters/ytelser.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { UNSAFE_Combobox } from '@navikt/ds-react';
+import { useSearchParams } from 'next/navigation';
+import { Suspense, useMemo, useState } from 'react';
+import type { IYtelse } from '@/lib/server/types';
+
+export const YtelserInternal = ({ ytelser, onChange }: { ytelser: IYtelse[]; onChange: (value: string[]) => void }) => {
+  const options = useMemo(() => ytelser.map((ytelse) => ({ label: ytelse.navn, value: ytelse.id })), [ytelser]);
+  const searchParams = useSearchParams();
+  const selected = useMemo(
+    () =>
+      (searchParams.get('ytelser')?.split(',') || [])
+        .map((id) => options.find((option) => option.value === id))
+        .filter((v): v is NonNullable<typeof v> => Boolean(v)),
+    [options, searchParams],
+  );
+  const [value, setValue] = useState('');
+
+  const filteredOptions = useMemo(
+    () => options.filter((option) => option.label.toLowerCase().includes(value.toLowerCase())),
+    [options, value],
+  );
+
+  return (
+    <UNSAFE_Combobox
+      filteredOptions={filteredOptions}
+      value={value}
+      label="Ytelser"
+      isMultiSelect
+      onChange={setValue}
+      selectedOptions={selected}
+      options={options}
+      onToggleSelected={(option, isSelected) =>
+        isSelected
+          ? onChange([...selected.map((s) => s.value), option])
+          : onChange(selected.map((s) => s.value).filter((id) => id !== option))
+      }
+    />
+  );
+};
+
+export const Ytelser = ({ ytelser, onChange }: { ytelser: IYtelse[]; onChange: (value: string[]) => void }) => {
+  return (
+    <Suspense>
+      <YtelserInternal ytelser={ytelser} onChange={onChange} />
+    </Suspense>
+  );
+};
