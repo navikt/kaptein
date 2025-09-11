a9fb4e80fb5d2efff58175e28de7920924625c6a
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-11 08:39:00 +0200

    tilbakekreving

--- components/filters/tilbakekreving.tsx
+++ components/filters/tilbakekreving.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { BodyShort, HelpText, HStack, Label, ToggleGroup } from '@navikt/ds-react';
+import { BodyShort, HelpText, HStack, Label, List, ToggleGroup } from '@navikt/ds-react';
+import { ListItem } from '@navikt/ds-react/List';
 import { usePathname } from 'next/navigation';
 import { useQueryState } from 'nuqs';
+import { useMemo } from 'react';
 import { isTilbakekrevingFilter, parseAsTilbakekrevingFilter } from '@/app/custom-query-parsers';
 import { TilbakekrevingFilter } from '@/app/query-types';
 import { QueryParam } from '@/lib/types/query-param';
@@ -11,11 +13,22 @@ export const Tilbakekreving = () => {
   const [tilbakekreving, setTilbakekreving] = useQueryState(QueryParam.TILBAKEKREVING, parseAsTilbakekrevingFilter);
   const pathname = usePathname();
 
+  const label = useMemo(() => {
+    switch (pathname) {
+      case '/ferdigstilte':
+        return <HelpForFerdigstilte />;
+      case '/aktive':
+        return <HelpForAktive />;
+      default:
+        return 'Tilbakekreving';
+    }
+  }, [pathname]);
+
   return (
     <ToggleGroup
       value={tilbakekreving ?? TilbakekrevingFilter.MED}
       onChange={(v) => setTilbakekreving(isTilbakekrevingFilter(v) ? v : TilbakekrevingFilter.MED)}
-      label={pathname === '/ferdigstilte' ? <HelpForFerdigstilte /> : 'Tilbakekreving'}
+      label={label}
     >
       <ToggleGroup.Item value={TilbakekrevingFilter.MED}>Med tilbakekreving</ToggleGroup.Item>
       <ToggleGroup.Item value={TilbakekrevingFilter.UTEN}>Uten tilbakekreving</ToggleGroup.Item>
@@ -34,6 +47,27 @@ const HelpForFerdigstilte = () => (
       <BodyShort spacing>Vis alle saker, uten de som er registrert som tilbakekrevingssak.</BodyShort>
       <Label>Kun tilbakekreving</Label>
       <BodyShort spacing>Vis kun saker som er registrert som tilbakekrevingssak.</BodyShort>
+    </HelpText>
+  </HStack>
+);
+
+const HelpForAktive = () => (
+  <HStack gap="2">
+    Tilbakekreving
+    <HelpText placement="right">
+      <BodyShort>
+        Sak satt med minst én av følgende innsendingshjemler regnes som aktiv tilbakekrevingssak i Kaptein:
+        <List>
+          <ListItem>folketrygdloven 22-15</ListItem>
+          <ListItem>folketrygdloven 22-15 dødsbo</ListItem>
+          <ListItem>alle andre varianter av folketrygdloven 22-15</ListItem>
+          <ListItem>folketrygdloven 22-17a</ListItem>
+          <ListItem>folketrygdloven 4-28</ListItem>
+          <ListItem>barnetrygdloven § 13</ListItem>
+          <ListItem>kontantstøtteloven § 11</ListItem>
+          <ListItem>lov om supplerende stønad § 13</ListItem>
+        </List>
+      </BodyShort>
     </HelpText>
   </HStack>
 );
