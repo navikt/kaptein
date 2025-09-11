882fa0d303beafdda45e68a75a2d8b72d3bfd7a1
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-11 08:26:58 +0200

    Utfall

--- components/filters/sakstyper-and-utfall.tsx
+++ components/filters/sakstyper-and-utfall.tsx
@@ -15,29 +15,32 @@ export const SakstyperAndUtfall = ({ sakstyperToUtfall }: Props) => {
   const [selectedSakstyper, setSelectedSakstyper] = useQueryState(QueryParam.SAKSTYPER, parseAsArrayOf(parseAsString));
   const [selectedUtfall, setSelectedUtfall] = useQueryState(QueryParam.UTFALL, parseAsArrayOf(parseAsString));
 
-  const sakstyperOptions = useMemo(
-    () =>
-      sakstyperToUtfall
-        .filter(({ id }) => id !== ANKE_I_TRYGDERETTEN_ID)
-        .map(({ navn, id }) => ({ label: navn, value: id })),
+  const relevantSakstyper = useMemo(
+    () => sakstyperToUtfall.filter((s) => s.id !== ANKE_I_TRYGDERETTEN_ID),
     [sakstyperToUtfall],
+  );
+
+  const sakstyperOptions = useMemo(
+    () => relevantSakstyper.map(({ navn, id }) => ({ label: navn, value: id })),
+    [relevantSakstyper],
   );
 
   const utfallOptions = useMemo(() => {
     const uniqueUtfall: Record<string, string> = {};
 
-    const allUtfall = sakstyperToUtfall
-      .filter((u) => selectedSakstyper === null || selectedSakstyper.length === 0 || selectedSakstyper.includes(u.id))
-      .flatMap((u) => u.utfall);
+    const relevantUtfall =
+      selectedSakstyper === null || selectedSakstyper.length === 0
+        ? relevantSakstyper.flatMap((u) => u.utfall)
+        : relevantSakstyper.filter((u) => selectedSakstyper.includes(u.id)).flatMap((u) => u.utfall);
 
-    for (const { id, navn } of allUtfall) {
+    for (const { id, navn } of relevantUtfall) {
       uniqueUtfall[id] = navn;
     }
 
     return Object.entries(uniqueUtfall)
       .map(([value, label]) => ({ label, value }))
       .toSorted((a, b) => a.label.localeCompare(b.label));
-  }, [sakstyperToUtfall, selectedSakstyper]);
+  }, [relevantSakstyper, selectedSakstyper]);
 
   return (
     <>
