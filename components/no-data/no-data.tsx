5f7138dc9f74de261e35397b1e7d56eb85137381
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-11 14:03:35 +0200

    Cosmetic

--- components/no-data/no-data.tsx
+++ components/no-data/no-data.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const NoData = ({ title }: Props) => (
   <VStack className="relative h-full pt-6" align="center">
-    <BarChartIcon fontSize={600} aria-hidden className="absolute z-0" color="var(--ax-bg-neutral-moderate)" />
+    <BarChartIcon fontSize={450} aria-hidden className="absolute z-0" color="var(--ax-bg-neutral-moderate)" />
     <Heading size="small">{title}</Heading>
     <VStack align="center" justify="center" className="z-1 grow">
       <Alert variant="info">Ingen data</Alert>
