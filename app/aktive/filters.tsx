96eefaa07687d40d10f8392f37629cf0717131bb
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-11 09:10:15 +0200

    No utfall for aktive

--- app/aktive/filters.tsx
+++ app/aktive/filters.tsx
@@ -1,22 +1,22 @@
 import { Reset } from '@/app/aktive/reset';
 import { FilterWrapper } from '@/components/filters/filter-wrapper';
 import { Klageenheter } from '@/components/filters/klageenheter';
-import { SakstyperAndUtfall } from '@/components/filters/sakstyper-and-utfall';
+import { Sakstyper } from '@/components/filters/sakstyper';
 import { Tilbakekreving } from '@/components/filters/tilbakekreving';
 import { Tildeling } from '@/components/filters/tildeling';
 import { YtelserAndInnsendingshjemler } from '@/components/filters/ytelser-and-hjemler/ytelser-and.hjemler';
-import { getKlageenheter, getSakstyperToUtfall, getYtelser } from '@/lib/server/api';
+import { getKlageenheter, getSakstyper, getYtelser } from '@/lib/server/api';
 
 export const Filters = async () => {
   const ytelser = await getYtelser();
   const klageenheter = await getKlageenheter();
-  const sakstyperToUtfall = await getSakstyperToUtfall();
+  const sakstyper = await getSakstyper();
 
   return (
     <FilterWrapper>
       <Reset />
       <Klageenheter klageenheter={klageenheter} />
-      <SakstyperAndUtfall sakstyperToUtfall={sakstyperToUtfall} />
+      <Sakstyper sakstyper={sakstyper} />
       <YtelserAndInnsendingshjemler ytelser={ytelser} />
       <Tildeling />
       <Tilbakekreving />
