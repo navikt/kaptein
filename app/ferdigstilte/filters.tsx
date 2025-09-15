c7ad2573a772edcecd22f7d4a1043aea0319a43c
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-15 09:58:56 +0200

    Ferdigstilte: Add klageenhet filter

--- app/ferdigstilte/filters.tsx
+++ app/ferdigstilte/filters.tsx
@@ -2,11 +2,12 @@ import { Suspense } from 'react';
 import { Reset } from '@/app/ferdigstilte/reset';
 import { DateRange } from '@/components/filters/date-range';
 import { FilterWrapper } from '@/components/filters/filter-wrapper';
+import { Klageenheter } from '@/components/filters/klageenheter';
 import { SakstyperAndUtfall } from '@/components/filters/sakstyper-and-utfall';
 import { HelpForFerdigstilte, Tilbakekreving } from '@/components/filters/tilbakekreving';
 import { YtelserAndRegistreringshjemler } from '@/components/filters/ytelser-and-hjemler/ytelser-and.hjemler';
-import { getLovkildeToRegistreringshjemler, getSakstyperToUtfall, getYtelser } from '@/lib/server/api';
-import type { IKodeverkValue, IYtelse, SakstypeToUtfall } from '@/lib/server/types';
+import { getKlageenheter, getLovkildeToRegistreringshjemler, getSakstyperToUtfall, getYtelser } from '@/lib/server/api';
+import type { IKodeverkSimpleValue, IKodeverkValue, IYtelse, SakstypeToUtfall } from '@/lib/server/types';
 
 export const Filters = async () => (
   <Suspense fallback={<RenderFilters />}>
@@ -18,12 +19,14 @@ const AsyncFilters = async () => {
   const ytelser = await getYtelser();
   const lovkildeToRegistreringshjemler = await getLovkildeToRegistreringshjemler();
   const sakstyperToUtfall = await getSakstyperToUtfall();
+  const klageEnheter = await getKlageenheter();
 
   return (
     <RenderFilters
       ytelser={ytelser}
       lovkildeToRegistreringshjemler={lovkildeToRegistreringshjemler}
       sakstyperToUtfall={sakstyperToUtfall}
+      klageenheter={klageEnheter}
     />
   );
 };
@@ -32,12 +35,19 @@ interface Props {
   ytelser?: IYtelse[];
   lovkildeToRegistreringshjemler?: IKodeverkValue<string>[];
   sakstyperToUtfall?: SakstypeToUtfall[];
+  klageenheter?: IKodeverkSimpleValue<string>[];
 }
 
-const RenderFilters = ({ ytelser = [], lovkildeToRegistreringshjemler = [], sakstyperToUtfall = [] }: Props) => (
+const RenderFilters = ({
+  ytelser = [],
+  lovkildeToRegistreringshjemler = [],
+  sakstyperToUtfall = [],
+  klageenheter = [],
+}: Props) => (
   <FilterWrapper>
     <Reset />
     <DateRange />
+    <Klageenheter klageenheter={klageenheter} />
     <SakstyperAndUtfall sakstyperToUtfall={sakstyperToUtfall} />
     <YtelserAndRegistreringshjemler ytelser={ytelser} lovkildeToRegistreringshjemler={lovkildeToRegistreringshjemler} />
     <Tilbakekreving help={<HelpForFerdigstilte />} />
