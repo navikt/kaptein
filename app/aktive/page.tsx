1b2037110620683f78110de3479e314953d991e2
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-09-16 13:57:53 +0200

    Client fetching WIP

--- app/aktive/page.tsx
+++ app/aktive/page.tsx
@@ -1,5 +1,9 @@
 import { Behandlinger } from '@/app/aktive/behandlinger';
+import { getSakstyper, getYtelser } from '@/lib/server/api';
 
 export default async function Page() {
-  return <Behandlinger />;
+  const ytelser = await getYtelser();
+  const sakstyper = await getSakstyper();
+
+  return <Behandlinger sakstyper={sakstyper} ytelser={ytelser} />;
 }
--- components/graphs/tildelte-saker-per-klageenhet/data.ts
+++ components/graphs/tildelte-saker-per-klageenhet/data.ts
@@ -2,7 +2,10 @@ import type { Data, State } from '@/components/graphs/tildelte-saker-per-klageen
 import type { GetGraphStateFn } from '@/lib/graphs';
 import type { Behandling, IKodeverkSimpleValue } from '@/lib/server/types';
 
-export const getTildelteSakerPerKlageenhetState: GetGraphStateFn<State> = ({ behandlinger, klageenheter }) => {
+export const getTildelteSakerPerKlageenhetState: GetGraphStateFn<State, Behandling> = ({
+  behandlinger,
+  klageenheter,
+}) => {
   const data = getData(behandlinger, klageenheter);
 
   const labels = data.map((d) => d.name);
