e6ba91c932f6eff4cf8bcabd638c37fc8251d889
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-09-30 16:11:35 +0200

    Split på vent pie piece into specific reasons

--- components/charts/tildelte-saker-på-vent-ikke-på-vent.tsx
+++ components/charts/tildelte-saker-på-vent-ikke-på-vent.tsx
@@ -4,34 +4,44 @@ import { useMemo } from 'react';
 import { COMMON_PIE_CHART_PROPS, COMMON_PIE_CHART_SERIES_PROPS } from '@/components/charts/common/common-chart-props';
 import { NoData } from '@/components/no-data/no-data';
 import { EChart } from '@/lib/echarts/echarts';
-import type { TildeltBehandling } from '@/lib/server/types';
+import type { IKodeverkValue, PåVentReason, TildeltBehandling } from '@/lib/server/types';
 
 interface Props {
   behandlinger: TildeltBehandling[];
+  påVentReasons: IKodeverkValue[];
 }
 
 const TITLE = 'Tildelte saker på vent / ikke på vent';
+const IKKE_PÅ_VENT_DESCRIPTION = 'Ikke på vent';
+const IKKE_PÅ_VENT_KEY = '-1';
 
-export const TildelteSakerPåVentIkkePåVent = ({ behandlinger }: Props) => {
+export const TildelteSakerPåVentIkkePåVent = ({ behandlinger, påVentReasons }: Props) => {
   const data = useMemo(() => {
-    const map = behandlinger.reduce<Map<boolean, { value: number; name: string }>>((acc, curr) => {
-      const value = curr.sattPaaVent === null;
-      const existing = acc.get(value);
+    const map = behandlinger.reduce<Map<PåVentReason | typeof IKKE_PÅ_VENT_KEY, { value: number; name: string }>>(
+      (acc, curr) => {
+        const { sattPaaVent } = curr;
+        const value = sattPaaVent === null ? IKKE_PÅ_VENT_KEY : sattPaaVent.reasonId;
+        const existing = acc.get(value);
 
-      if (existing) {
-        existing.value += 1;
-      } else {
-        acc.set(value, {
-          name: value ? 'Ikke på vent' : 'På vent',
-          value: 1,
-        });
-      }
+        if (existing !== undefined) {
+          existing.value += 1;
+        } else {
+          acc.set(value, {
+            name:
+              sattPaaVent === null
+                ? IKKE_PÅ_VENT_DESCRIPTION
+                : (påVentReasons.find((r) => r.id === sattPaaVent.reasonId)?.beskrivelse ?? 'Ukjent'),
+            value: 1,
+          });
+        }
 
-      return acc;
-    }, new Map());
+        return acc;
+      },
+      new Map(),
+    );
 
     return Object.values(Object.fromEntries(map));
-  }, [behandlinger]);
+  }, [behandlinger, påVentReasons]);
 
   if (behandlinger.length === 0) {
     return <NoData title={TITLE} />;
@@ -47,7 +57,8 @@ export const TildelteSakerPåVentIkkePåVent = ({ behandlinger }: Props) => {
             ...COMMON_PIE_CHART_SERIES_PROPS,
             data,
             label: {
-              formatter: ({ name, value }: { name: string; value: number }) => `${name}: ${value}`,
+              formatter: ({ name, value }: { name: string; value: number }) =>
+                `${name}: ${value} (${((value / behandlinger.length) * 100).toFixed(1)}%)`,
             },
           },
         ],
