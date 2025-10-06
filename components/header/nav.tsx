67d17ad9f4cc922557999c675028218eee9b2c26
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-10-06 09:55:19 +0200

    Fix default params for saksstrøm

--- components/header/nav.tsx
+++ components/header/nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { InternalHeader } from '@navikt/ds-react/InternalHeader';
-import { format, startOfMonth } from 'date-fns';
+import { endOfMonth, format, startOfMonth, subMonths } from 'date-fns';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useCallback, useMemo } from 'react';
@@ -14,15 +14,15 @@ const TODAY = new Date();
 const DEFAULT_TO = format(TODAY, ISO_DATE_FORMAT);
 const DEFAULT_FROM = format(startOfMonth(TODAY), ISO_DATE_FORMAT);
 
+const SAKSSTRØM_DEFAULT_FROM = format(startOfMonth(subMonths(TODAY, 4)), ISO_DATE_FORMAT);
+const SAKSSTRØM_DEFAULT_TO = format(endOfMonth(subMonths(TODAY, 1)), ISO_DATE_FORMAT);
+
 export const Nav = () => {
   const pathname = usePathname();
   const params = useSearchParams();
 
   const setDefaultParams = useCallback((searchParams: URLSearchParams) => {
     searchParams.get(QueryParam.TILDELING) ?? searchParams.set(QueryParam.TILDELING, TildelingFilter.ALL);
-    searchParams.get(QueryParam.ALDER_MAX_DAYS) ?? searchParams.set(QueryParam.ALDER_MAX_DAYS, '84');
-    searchParams.get(QueryParam.ALDER_PER_YTELSE_MAX_DAYS) ??
-      searchParams.set(QueryParam.ALDER_PER_YTELSE_MAX_DAYS, '84');
   }, []);
 
   const aktiveParams = useMemo(() => {
@@ -31,6 +31,10 @@ export const Nav = () => {
     searchParams.delete(QueryParam.FROM);
     searchParams.delete(QueryParam.TO);
     searchParams.delete(QueryParam.REGISTRERINGSHJEMLER);
+
+    searchParams.get(QueryParam.ALDER_MAX_DAYS) ?? searchParams.set(QueryParam.ALDER_MAX_DAYS, '84');
+    searchParams.get(QueryParam.ALDER_PER_YTELSE_MAX_DAYS) ??
+      searchParams.set(QueryParam.ALDER_PER_YTELSE_MAX_DAYS, '84');
 
     setDefaultParams(searchParams);
 
@@ -45,6 +49,9 @@ export const Nav = () => {
 
     searchParams.get(QueryParam.FROM) ?? searchParams.set(QueryParam.FROM, DEFAULT_FROM);
     searchParams.get(QueryParam.TO) ?? searchParams.set(QueryParam.TO, DEFAULT_TO);
+    searchParams.get(QueryParam.ALDER_MAX_DAYS) ?? searchParams.set(QueryParam.ALDER_MAX_DAYS, '84');
+    searchParams.get(QueryParam.ALDER_PER_YTELSE_MAX_DAYS) ??
+      searchParams.set(QueryParam.ALDER_PER_YTELSE_MAX_DAYS, '84');
 
     setDefaultParams(searchParams);
 
@@ -54,8 +61,11 @@ export const Nav = () => {
   const saksstrømParams = useMemo(() => {
     const searchParams = new URLSearchParams(params.toString());
 
-    searchParams.get(QueryParam.FROM) ?? searchParams.set(QueryParam.FROM, DEFAULT_FROM);
-    searchParams.get(QueryParam.TO) ?? searchParams.set(QueryParam.TO, DEFAULT_TO);
+    searchParams.get(QueryParam.FROM) ?? searchParams.set(QueryParam.FROM, SAKSSTRØM_DEFAULT_FROM);
+    searchParams.get(QueryParam.TO) ?? searchParams.set(QueryParam.TO, SAKSSTRØM_DEFAULT_TO);
+
+    searchParams.delete(QueryParam.ALDER_MAX_DAYS);
+    searchParams.delete(QueryParam.ALDER_PER_YTELSE_MAX_DAYS);
 
     setDefaultParams(searchParams);
 
