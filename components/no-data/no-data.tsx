b47a32f211613a46eca6f0e5a391b5ba2ea9df5b
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-10-10 14:27:38 +0200

    Fix alignment for no data

--- components/no-data/no-data.tsx
+++ components/no-data/no-data.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 export const NoData = ({ title }: Props) => (
-  <VStack className="relative h-full pt-6" align="center">
+  <VStack className="relative h-full pt-6" align="center" justify="center">
     <BarChartIcon fontSize={450} aria-hidden className="absolute z-0" color="var(--ax-bg-neutral-moderate)" />
     <Heading size="small">{title}</Heading>
     <VStack align="center" justify="center" className="z-1 grow">
