3093fb953253336c0f239ca2c5bdb893fe06200d
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-10-27 10:31:12 +0100

    Fix wrong z-index in no-data component

--- components/no-data/no-data.tsx
+++ components/no-data/no-data.tsx
@@ -8,7 +8,9 @@ interface Props {
 export const NoData = ({ title }: Props) => (
   <VStack className="relative h-full pt-6" align="center" justify="center">
     <BarChartIcon fontSize={450} aria-hidden className="absolute z-0" color="var(--ax-bg-neutral-moderate)" />
-    <Heading size="small">{title}</Heading>
+    <Heading size="small" className="z-1">
+      {title}
+    </Heading>
     <VStack align="center" justify="center" className="z-1 grow">
       <Alert variant="info">Ingen data</Alert>
     </VStack>
