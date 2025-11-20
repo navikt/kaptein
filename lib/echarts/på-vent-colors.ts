4c166dd72be528170511edb5a909fc74261dd526
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-11-20 14:30:04 +0100

    Add new på vent reasons

--- lib/echarts/på-vent-colors.ts
+++ lib/echarts/på-vent-colors.ts
@@ -8,4 +8,6 @@ export const PÅ_VENT_COLORS: Record<PåVentReason, ColorToken> = {
   [PåVentReason.SATT_I_BERO]: ColorToken.Magenta500,
   [PåVentReason.ANNET_TIL_FORELEGGELSE]: ColorToken.Neutral500,
   [PåVentReason.ANNET]: ColorToken.Purple500,
+  [PåVentReason.VENTER_PAA_UTFYLLENDE_KLAGE]: ColorToken.Lime500,
+  [PåVentReason.VENTER_PAA_UTFYLLENDE_ANKE]: ColorToken.Beige500,
 };
